4f862b23611542a0eaccfccafac2ced638940df0
shubhalakshmi B S <shubhalakshmibs@nousinfo.com>
2024-01-23 11:27:19 +0530

    Req 333: checking comments

--- newCom.py
+++ newCom.py
@@ -1,6 +1,8 @@
 import openai
+
+#This function call GPT to run given prompt
 def openaiDoc(changes_in_file):
-    openai.api_key = ""
+    openai.api_key = "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"
     prompt = ""
     response = openai.Completion.create(
         engine="gpt-3.5-turbo-instruct",
@@ -16,9 +18,9 @@ def openaiDoc(changes_in_file):
     return generated_documentation
 
 
-
 import re
 
+#function to extract reqId/defectId
 def extract_req_id_defect_id(commit_message):
     pattern = r'\b(Req \d+|Defect \d+)\b'
     matches = re.findall(pattern, commit_message)
